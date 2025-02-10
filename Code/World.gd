67386a0df99c538d87f71604ab5ee4c39f3e22ce
DevSkully <dev.skully01@gmail.com>
2025-02-10 22:22:23 +0800

    Something

--- Code/World.gd
+++ Code/World.gd
@@ -3,9 +3,22 @@ extends Node2D
 const enemy = preload("res://tscn/Object/Enemy.tscn")
 
 @onready var player = get_node("Player")
+@onready var camera = player.get_node("Camera2D")
 
-func _spawnEnemy()->void:
-	pass
+func _ready() -> void:
+	var tileSet = get_node("newTileSet")
+	print(tileSet.get_node('TileMap').get_used_rect().size)
+
+func _spawnEnemy()->Vector2:
+	## CAMERA BOUNDS
+	var half_size = camera.get_viewport_rect().size / 2 / camera.zoom
+	var bounds = Rect2(camera.global_position - half_size, half_size*2)
+	
+	var spawnPosition:Vector2 = Vector2.ZERO
+	
+	
+	
+	return spawnPosition
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("EnemySpawn"):
